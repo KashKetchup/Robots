59662f52621ed5e5a99e0808f888a3d5b2089d26
Никита <nanedoshopa@gmail.com>
2026-03-24 10:29:51 +0500

    Автоматическое создание папки и файла state.cfg

--- src/main/java/gui/FileHandler.java
+++ src/main/java/gui/FileHandler.java
@@ -1,6 +1,8 @@
 package gui;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,13 +19,18 @@ public class FileHandler {
      * Записать состояния в файл
      */
     public void writeWindowStates(Map<String, LastWindowState> windowStates) {
-        String dir = System.getProperty("user.home") + "/nedoshopa/state.cfg";
+        String dir = System.getProperty("user.home") + "/nedoshopa";
         File file = new File(dir);
         if (!file.exists()) {
-            file.mkdir();
+            try {
+                file.mkdir();
+                Files.createFile(Path.of(dir+"/state.cfg"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(dir));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(dir+"/state.cfg"));
             for (String key : windowStates.keySet()) {
                 LastWindowState curWindowState = windowStates.getOrDefault(key,
                         new LastWindowState(0, 0, 0, 0, false));
@@ -42,24 +49,31 @@ public class FileHandler {
         String dir = System.getProperty("user.home") + "/nedoshopa/state.cfg";
         Map<String, LastWindowState> result = new HashMap<>();
         File file = new File(dir);
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(dir));
-            if (file.exists()) {
-                for (String key : keys) {
-                    String in = bufferedReader.readLine();
-                    if (in != null) {
-                        String[] tmp = in.split("\\s");
-                        result.put(key, new LastWindowState(
-                                toInt(tmp[0]), toInt(tmp[1]), toInt(tmp[2]), toInt(tmp[3]),
-                                tmp[4] == "1" ? true : false));
-                    } else {
-                        result.put(key, new LastWindowState(-1, -1, -1, -1, false));
+        if (!file.exists()) {
+            for (String key : keys) {
+                result.put(key, new LastWindowState(-1, -1, -1, -1, false));
+            }
+        } else {
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(dir));
+                if (file.exists()) {
+                    for (String key : keys) {
+                        String in = bufferedReader.readLine();
+                        if (in != null) {
+                            String[] tmp = in.split("\\s");
+                            result.put(key, new LastWindowState(
+                                    toInt(tmp[0]), toInt(tmp[1]), toInt(tmp[2]), toInt(tmp[3]),
+                                    tmp[4] == "1" ? true : false));
+                        } else {
+                            result.put(key, new LastWindowState(-1, -1, -1,
+                                    -1, false));
+                        }
                     }
                 }
+            } catch (IOException e) {
+                throw new IOException(
+                        "Couldn't read state from file: " + e.getMessage());
             }
-        } catch (IOException e) {
-            throw new IOException(
-                    "Couldn't read state from file: " + e.getMessage());
         }
         return result;
     }
